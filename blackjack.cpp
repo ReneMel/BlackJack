0cb22bac27de022b596c3c5fe5e400e98093460b
Rene Melendez <rene.melendeza@gmail.com>
2018-11-18 19:44:32 -0600

    Bugs arreglados

--- blackjack.cpp
+++ blackjack.cpp
@@ -10,23 +10,219 @@ int rev;
 int op;
 bool endgame=false;
 bool banca=false;
+int as=4;
+int dos=4;
+int tres=4;
+int cuatro=4;
+int cinco=4;
+int seis=4;
+int siete=4;
+int ocho=4;
+int nueve=4;
+int diez=16;
+int Probnextmov;
+int ProbBlack;
 
 
 
-int probabilidad(){
+void Probabilidad(int CartaJ1, int CartaJ2, int TotalJugador){
+	//eliminar cartas que el jugador ha sacado del tablero 
+	float CasosPosibles=0;
+	float CasosFavorables=0;
+	
+	//op=2;
 
+	if (as!=0){
+		if(CartaJ1==1){
+		as--;
+		}
+	}
 
-};
+	if (dos!=0){
+		if(CartaJ1==2){
+		dos--;
+		}
+	}
+	if (tres!=0){
+		if(CartaJ1==3){
+		tres--;
+		}
+	}
+	if (cuatro!=0){
+		if(CartaJ1==4){
+		cuatro--;
+		}
+	}
+	if (cinco!=0){
+		if(CartaJ1==5){
+		cinco--;
+		}
+	}
+	if (seis!=0){
+		if(CartaJ1==6){
+		seis--;
+		}
+	}
+	if (siete!=0){
+		if(CartaJ1==7){
+		siete--;
+		}
+	}
+	if (ocho!=0){
+		if(CartaJ1==8){
+		ocho--;
+		}
+	}
+	if (nueve!=0){
+		if(CartaJ1==9){
+		nueve--;
+		}
+	}
+	if (diez!=0){
+		if(CartaJ1==10){
+		diez--;
+		}
+	}
 
-/*int numeroX(){
-	srand(time(0));
-	numrandom=1+(rand()%10);
-	return numrandom;
-}*/
+	// carta 2, la cual solo funcionará en la primera tirada 
+	if (CartaJ2!=0){
+		if (as!=0){
+				if(CartaJ1==1){
+				as--;
+				}
+		}
+
+		if (dos!=0){
+				if(CartaJ1==2){
+				dos--;
+				}
+		}
+		if (tres!=0){
+				if(CartaJ1==3){
+				tres--;
+				}
+		}
+		if (cuatro!=0){
+				if(CartaJ1==4){
+				cuatro--;
+				}
+		}
+		if (cinco!=0){
+				if(CartaJ1==5){
+				cinco--;
+				}
+		}
+		if (seis!=0){
+				if(CartaJ1==6){
+				seis--;
+				}
+		}
+		if (siete!=0){
+				if(CartaJ1==7){
+				siete--;
+				}
+		}
+		if (ocho!=0){
+				if(CartaJ1==8){
+				ocho--;
+				}
+		}
+		if (nueve!=0){
+			if(CartaJ1==9){
+			nueve--;
+			}
+		}
+		if (diez!=0){
+			if(CartaJ1==10){
+			diez--;
+			}
+		}
+	}
+
+	CasosPosibles=as+dos+tres+cuatro+cinco+seis+siete+ocho+nueve+diez;
+	cout<< "CASOS POSIBLES: "<<CasosPosibles<<endl;
+
+	if(TotalJugador+1<=21){
+		CasosFavorables+=as;
+	}
+	if(TotalJugador+2<=21){
+		CasosFavorables+=dos;
+	}
+	if (TotalJugador+3<=21){
+		CasosFavorables+=tres;
+	}
+	if (TotalJugador+4<=21){
+		CasosFavorables+=cuatro;
+	}
+	if (TotalJugador+5<=21){
+		CasosFavorables+=cinco;
+	}
+	if (TotalJugador+6<=21){
+		CasosFavorables+=seis;
+	}
+	if (TotalJugador+7<=21){
+		CasosFavorables+=siete;
+	}
+	if (TotalJugador+8<=21){
+		CasosFavorables+=ocho;
+	}
+	if (TotalJugador+9<=21){
+		CasosFavorables+=nueve;
+	}
+	if (TotalJugador+10<=21){
+		CasosFavorables+=diez;
+	}
+
+	cout<<"CASOS FAVORABLES: "<<CasosFavorables<<endl;
+
+	cout<<"La Probabilidad de exito en tu siguiente movimiento es del "<<(CasosFavorables/CasosPosibles)*100<<"%"<<endl;
+
+	float a =diez, c = CasosPosibles, d;
+		d = a / c;
+		//e = b / c;	
+	if (TotalJugador!=21){
+		cout<<"Probabilidad para completar el BlackJack!: "<<endl;
+	}
+
+	if (TotalJugador==2 or TotalJugador==3  or TotalJugador==4 or TotalJugador==5 or TotalJugador==6 or TotalJugador==7 or TotalJugador==8  or TotalJugador==9 or TotalJugador==10 or TotalJugador==11){
+		cout << "La probabilidad de que se obtenga un 10 es de:  " << d*100 << "%" << endl;
+	}
+	if (TotalJugador==12){
+		cout << "La probabilidad de que se obtenga un 9 es de: " << (nueve/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==13){
+		cout << "La probabilidad de que se obtenga un 8 es de: " << (ocho/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==14){
+		cout << "La probabilidad de que se obtenga un 7 es de: " << (siete/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==15){
+		cout << "La probabilidad de que se obtenga un 6 es de: " << (seis/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==16){
+		cout << "La probabilidad de que se obtenga un 5 es de: " << (cinco/c)*100 << "%" <<endl;
+	}
+	if (TotalJugador==17){
+		cout << "La probabilidad de que se obtenga un 4 es de: " << (cuatro/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==18){
+		cout << "La probabilidad de que se obtenga un 3 es de: " << (tres/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==19){
+		cout << "La probabilidad de que se obtenga un 2 es de: " << (dos/c)*100 << "%" << endl;
+	}
+	if (TotalJugador==20){
+		cout << "La probabilidad de que se obtenga un 1 es de: " << (as/c)*100 << "%" << endl;
+	}
+
+}
+
 
 void  PrimeraJugada(){
+		float a =16 ,b = 4, c = 52, d,e;
+	d = a / c;
+	e = b / c;
 	 op=2;
-
 
 	srand(time(NULL));
     int CartaJ1 = 1+(rand()%10);
@@ -34,17 +230,11 @@ void  PrimeraJugada(){
     int CartaB1 = 1+(rand()%10);
     int CartaB2 = 1+(rand()%10);
 
-		/*CartaJ1=numeroX();
-		//Tj+=CartaJ1;
-		CartaB1=numeroX();
-		//Tb+=CartaB1;
-
-		CartaJ2=numeroX();
-		CartaB2=numeroX();*/
 
 		cout<< "Tus primeras cartas son: "<< CartaJ1<<" y "<<CartaJ2<<endl;
 		Tj=CartaJ1+CartaJ2;
 		cout<<"Tu total es: "<<Tj<<endl;
+
 
 
 		//cout<< "Las primeras cartas de la banca son: "<<CartaB1<<" y "<<CartaB2<<endl;
@@ -52,6 +242,9 @@ void  PrimeraJugada(){
 		//cout<<"El total de la Banca es: "<<Tb<<endl<<endl;
 		cout<<"LA BANCA TOMA SUS PRIMERAS DOS CARTAS"<<endl;
 		cout<<"-------------------------------------"<<endl;
+		if (Tj<21){
+			Probabilidad(CartaJ1,CartaJ2,Tj);
+		}
 
 
 		cout <<"¿Cual es tu siguiente movimiento?"<<endl<<"Para seguir tomando cartas presiona 1, para quedarte con las que tienes presiona 0"<<endl;
@@ -73,7 +266,7 @@ void  PrimeraJugada(){
 		}
 
 	}
-
+	
 	//return op;
 
 }
@@ -84,6 +277,7 @@ void revisar(){
 		cout<< "Tu total fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
 
 	}
 	if (Tb==21){
@@ -92,6 +286,7 @@ void revisar(){
 		cout<< "Tu total Fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
 	}
 	if (Tj>21){
 		cout<<endl;
@@ -99,6 +294,7 @@ void revisar(){
 		cout<<"Tu total fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
 	}
 	if (Tb>21){
 		cout<<endl;
@@ -106,6 +302,7 @@ void revisar(){
 		cout<<"Tu total fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
 	}
 	if (Tj>21 && Tb>21){
 		cout<<endl;
@@ -113,6 +310,7 @@ void revisar(){
 		cout<<"Tu total fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
 	}
 
 	if (op==0){
@@ -122,6 +320,7 @@ void revisar(){
 				cout<< "Tu total Fue: "<<Tj<<endl;
 				cout<<"El total de la banca fue: " <<Tb<<endl;
 				endgame=true;
+				return;
 		}
 		if (Tj<Tb && Tb<=21){
 				cout<<endl;
@@ -129,6 +328,7 @@ void revisar(){
 				cout<< "Tu total Fue: "<<Tj<<endl;
 				cout<<"El total de la banca fue: " <<Tb<<endl;
 				endgame=true;
+				return;
 		}
 	  	if(Tj==Tb && Tj<=21 && Tb<=21){
 			cout<<endl;
@@ -136,6 +336,7 @@ void revisar(){
 			cout<<"Tu total fue: "<<Tj<<endl;
 			cout<<"El total de la banca fue: " <<Tb<<endl;
 			endgame=true;
+			return;
 		}
 		if(Tj>21 && Tb>21){
 		cout<<endl;
@@ -143,8 +344,9 @@ void revisar(){
 		cout<<"Tu total fue: "<<Tj<<endl;
 		cout<<"El total de la banca fue: " <<Tb<<endl;
 		endgame=true;
+		return;
+		}
 	}
-}
 }
 
 void revisarbanca(){
@@ -154,6 +356,9 @@ void revisarbanca(){
 }
 
 void Jugar(){
+	float a =16 ,b = 4, c = 52, d,e;
+	d = a / c;
+	e = b / c;
 	if (endgame!=true){
 		do {
 
@@ -168,8 +373,12 @@ void Jugar(){
 					cout<< "Tu carta es: "<< CartaJ1<<endl;
 					Tj+=CartaJ1;
 					cout<<"Tu total es: "<<Tj<<endl;
+					if (Tj<21){	
+						Probabilidad(CartaJ1,0,Tj);
+					}
 					int CartaB1 = 1+(rand()%10);
 				}
+
 
 				if (endgame!=true){
 					revisarbanca();
@@ -204,15 +413,11 @@ void Jugar(){
 								cout<<"Opcion no valida, vuelve a elegir."<<endl;
 						}
 				}
-
-
-
 		}while (op==1);
 		//revisar();
- 
-
 	}
 }
+
 
 
 int main(int argc, char const *argv[])
@@ -231,8 +436,12 @@ int main(int argc, char const *argv[])
 	cout<<endl;
 
 	PrimeraJugada();
+	
 	Jugar();
-	revisar();
+	if (endgame!=true){	
+		revisar();
+	}
+	
 
 
 	return 0;
